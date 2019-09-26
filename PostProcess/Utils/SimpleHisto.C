f9c009e1ae96d49360915c46363714445d707eee
SangEun Lee <d4space@gmail.com>
2019-09-26 20:34:03 +0900

    up

--- PostProcess/Utils/SimpleHisto.C
+++ PostProcess/Utils/SimpleHisto.C
@@ -4,25 +4,25 @@
   Events->Draw("GenEvtFlag");
   c1->SaveAs("HiMassCtlPlots/GenEvtFlag.png");
 
-  Events->Draw("GenDrAk8Lept","GenEvtFlag==1");
+  Events->Draw("GenDrAk8Lept","GenEvtFlag==1 && GenDrAk8Lept<5");
   c1->SaveAs("HiMassCtlPlots/GenDrAk8Lept_Flag1.png");
 
-  Events->Draw("GenDrAk8Ak4","GenEvtFlag==1");
+  Events->Draw("GenDrAk8Ak4","GenEvtFlag==1&&GenDrAk8Ak4<5");
   c1->SaveAs("HiMassCtlPlots/GenDrAk8Ak4_Flag1.png");
 
-  Events->Draw("GenW_Ak8_mass","GenEvtFlag==1");
+  Events->Draw("GenW_Ak8_mass","GenEvtFlag==1&&GenW_Ak8_mass<300");
   c1->SaveAs("HiMassCtlPlots/GenW_Ak8_mass_Flag1.png");
 
-  Events->Draw("GenW_Lept_mass","GenEvtFlag==1");
+  Events->Draw("GenW_Lept_mass","GenEvtFlag==1&&GenW_Lept_mass<150");
   c1->SaveAs("HiMassCtlPlots/GenW_Lept_Flag1.png");
 
   Events->Draw("GenH_mass","GenEvtFlag==1");
   c1->SaveAs("HiMassCtlPlots/GenH_mass_Flag1.png");
 
-  Events->Draw("GenW_Ak4_mass","GenEvtFlag==2");
+  Events->Draw("GenW_Ak4_mass","GenEvtFlag==2&&GenW_Ak4_mass<500");
   c1->SaveAs("HiMassCtlPlots/GenW_Ak4_mass_Flag2.png");
 
-  Events->Draw("GenW_Lept_mass","GenEvtFlag==2");
+  Events->Draw("GenW_Lept_mass","GenEvtFlag==2&&GenW_Lept_mass<150");
   c1->SaveAs("HiMassCtlPlots/GenW_Lept_Flag2.png");
 
   Events->Draw("GenH_mass","GenEvtFlag==2");
