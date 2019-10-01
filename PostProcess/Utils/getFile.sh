6c06687e7c3d80c5ecfb8a0badb04822b743310a
SangEun Lee <d4space@gmail.com>
2019-10-01 21:18:50 +0900

    up

--- PostProcess/Utils/getFile.sh
+++ PostProcess/Utils/getFile.sh
@@ -1,2 +1,4 @@
-fileName="nanoLatino_GluGluHToWWToLNuQQ_M4000__part6_Skim.root"
+fileName="nanoLatino_GluGluHToWWToLNuQQ_M900__part1_Skim.root"
+#fileName="nanoLatino_GluGluHToWWToLNuQQ_M2500__part0_Skim.root"
+#fileName="nanoLatino_GluGluHToWWToLNuQQ_M4000__part6_Skim.root"
 scp  -P 4280 ui20.sdfarm.kr:Latino/workdir/jobs/${fileName} .
