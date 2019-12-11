f62430e4490568dea72e2eb9afe2f4ec84c64c7b
SangEun Lee <d4space@gmail.com>
2019-12-11 22:00:58 +0900

    up

--- PlotConfigPlotMacro/interfPlots.py
+++ PlotConfigPlotMacro/interfPlots.py
@@ -17,7 +17,7 @@ from T800Tools  import *
 tdrStyle.setTDRStyle()
 
 
-inF = TakeFileHisto('plots2500_Int_2017.root')
+inF = TakeFileHisto('plots900_Int_2017.root')
 
 #################
 # H + I(with H)   
@@ -29,47 +29,47 @@ inF = TakeFileHisto('plots2500_Int_2017.root')
 cnvs = ROOT.TCanvas( 'tcanvas', 'distro',800,600)
 cnvs.cd()
 
-histoName="GenPass/GenRecH_m2500/histo_ggHWW_2500M"
-histo2500_ggHWW = inF.getHisto(histoName)
+histoName="GenResolved/GenRecH_m900/histo_ggHWW_900M"
+histo900_ggHWW = inF.getHisto(histoName)
 
-histoName="GenPass/GenRecH_m2500/histo_ggHWW_I_h_2500M"
-histo2500_ggHWW_I_h = inF.getHisto(histoName)
+histoName="GenResolved/GenRecH_m900/histo_ggHWW_I_h_900M"
+histo900_ggHWW_I_h = inF.getHisto(histoName)
 
-histoName="GenPass/GenRecH_m2500/histo_ggHWW_I_B_2500M"
-histo2500_ggHWW_I_B = inF.getHisto(histoName)
+histoName="GenResolved/GenRecH_m900/histo_ggHWW_I_B_900M"
+histo900_ggHWW_I_B = inF.getHisto(histoName)
 
-histoName="GenPass/GenRecH_m2500/histo_ggHWW_I_2500M"
-histo2500_ggHWW_I = inF.getHisto(histoName)
+histoName="GenResolved/GenRecH_m900/histo_ggHWW_I_900M"
+histo900_ggHWW_I = inF.getHisto(histoName)
 
 
-histo2500_ggHWW.SetLineWidth(2)
-histo2500_ggHWW.SetLineColor(ROOT.kBlack)
-histo2500_ggHWW_I.SetLineColor(ROOT.kGreen)
-histo2500_ggHWW_I_h.SetLineColor(ROOT.kRed)
-histo2500_ggHWW_I_B.SetLineColor(ROOT.kBlue)
-histo2500_ggHWW.SetMaximum(12500)
-histo2500_ggHWW.SetMinimum(-300)
+histo900_ggHWW.SetLineWidth(2)
+histo900_ggHWW.SetLineColor(ROOT.kBlack)
+histo900_ggHWW_I.SetLineColor(ROOT.kGreen)
+histo900_ggHWW_I_h.SetLineColor(ROOT.kRed)
+histo900_ggHWW_I_B.SetLineColor(ROOT.kBlue)
+histo900_ggHWW.SetMaximum(50)
+histo900_ggHWW.SetMinimum(-40)
 
-histo2500_ggHWW.SetXTitle("m(lnjj) (GeV)");
-histo2500_ggHWW.GetXaxis().SetTitleSize(0.035);
-histo2500_ggHWW.GetXaxis().SetLabelSize(0.03);
-histo2500_ggHWW.GetXaxis().SetTitleOffset(1.3);
+histo900_ggHWW.SetXTitle("m(lnjj) (GeV)");
+histo900_ggHWW.GetXaxis().SetTitleSize(0.035);
+histo900_ggHWW.GetXaxis().SetLabelSize(0.03);
+histo900_ggHWW.GetXaxis().SetTitleOffset(1.3);
 
-histo2500_ggHWW.Draw('h')
-histo2500_ggHWW_I_h.Draw('hsame')
-histo2500_ggHWW_I_B.Draw('hsame')
-histo2500_ggHWW_I.Draw('hsame')
+histo900_ggHWW.Draw('h')
+histo900_ggHWW_I_h.Draw('hsame')
+histo900_ggHWW_I_B.Draw('hsame')
+histo900_ggHWW_I.Draw('hsame')
 
 leg= ROOT.TLegend(0.7,0.7,0.9,0.9);
 leg.SetFillColor(0);
 leg.SetBorderSize(0);
 #leg.SetTextFont(8);
-leg.AddEntry(histo2500_ggHWW,  "ggH(2500)WW","f");
-leg.AddEntry(histo2500_ggHWW_I_h, "ggI_Hh","f");
-leg.AddEntry(histo2500_ggHWW_I_B, "ggI_HB","f");
-leg.AddEntry(histo2500_ggHWW_I,"ggI_HB+Hh","f");
+leg.AddEntry(histo900_ggHWW,  "ggH(900)WW","f");
+leg.AddEntry(histo900_ggHWW_I_h, "ggI_Hh","f");
+leg.AddEntry(histo900_ggHWW_I_B, "ggI_HB","f");
+leg.AddEntry(histo900_ggHWW_I,"ggI_HB+Hh","f");
 leg.Draw("same")
 
-cnvs.SaveAs("GenPassH2500HaIHhB.png")
-cnvs.SaveAs("GenPassH2500HaIHhB.pdf")
+cnvs.SaveAs("GenResolvedH900HaIHhB.png")
+cnvs.SaveAs("GenResolvedH900HaIHhB.pdf")
 
