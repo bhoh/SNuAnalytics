7f2635726b0a2e5a3309c3b31c9623e497e58f5e
ByungHun Oh <bh.oh@cern.ch>
2020-02-25 16:11:11 +0900

    place holder for ChargedHiggsToCB analysis

---
+++ PostProcess/ChargedHiggsToCB/2017/run_signal.sh
@@ -0,0 +1,46 @@
+# /xrootd//store/user/jhchoi/Latino/HWWNano/
+# Sites_cfg: /xrd/store/group/phys_higgs/cmshww/jhchoi/Latino/HWWNano/
+# changed to /xrd/store/user/jhchoi/Latino/HWWNano/
+# L1Loose
+
+SAMPLES=(
+CHToCB_M050
+CHToCB_M060
+CHToCB_M070
+CHToCB_M075
+CHToCB_M080
+CHToCB_M085
+CHToCB_M090
+CHToCB_M100
+CHToCB_M110
+CHToCB_M120
+CHToCB_M130
+CHToCB_M140
+CHToCB_M150
+)
+
+EXCLUDE=()
+
+#--Run--#
+SAMPLE_LIST=''
+EXCLUDE_LIST=''
+for s in ${SAMPLES[@]};do SAMPLE_LIST=${s}','${SAMPLE_LIST};done
+for e in ${EXCLUDE[@]};do EXCLUDE_LIST=${e}','${EXCLUDE_LIST};done
+#echo ${SAMPLE_LIST}
+#echo ${EXCLUDE_LIST}
+
+######################
+# Option: -R(redo)
+######################
+
+sitescfg='--sitescfg SNuAnalytics/PostProcess/ChargedHiggsToCB/Sites_cfg.py'
+modcfg='--modcfg SNuAnalytics/PostProcess/ChargedHiggsToCB/Steps_cfg.py'
+datacfg='--datacfg SNuAnalytics/PostProcess/ChargedHiggsToCB/Productions_cfg.py'
+
+#--l1Prod--#
+#mkPostProc.py ${sitescfg} ${modcfg} ${datacfg} -p Fall2017_102X_nAODv4_Full2017v5 -i Prod -s MCl1loose2017v5 -b -T ${SAMPLE_LIST}
+#--Corr--#
+#mkPostProc.py ${sitescfg} ${modcfg} ${datacfg} -p Fall2017_102X_nAODv4_Full2017v5 -i MCl1loose2017v5 -s MCCorr2017v5 -b -T ${SAMPLE_LIST} 
+
+
+mkPostProc.py ${sitescfg} ${modcfg} ${datacfg} -p Fall2017_102X_nAODv4_Full2017v5 -i MCl1loose2017v5__MCCorr2017v5 -s Semilep2017 -b -T ${SAMPLE_LIST}
