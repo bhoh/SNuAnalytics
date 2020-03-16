e805e97141a8912c2d18beba210b08c3b11dedb9
ByungHun Oh <bh.oh@cern.ch>
2020-03-16 20:31:21 +0900

    get jme pt jer

--- NanoGardenerModules/KinFitterProducer.py
+++ NanoGardenerModules/KinFitterProducer.py
@@ -11,13 +11,15 @@
 
 
 import ROOT
+import math, os, tarfile, tempfile, shutil
+import numpy as np
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 
-from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection 
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection, Object
 from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
-from PhysicsTools.NanoAODTools.postprocessing.modules.common.collectionMerger import collectionMerger
+from PhysicsTools.NanoAODTools.postprocessing.tools import matchObjectCollection, matchObjectCollectionMultiple
+#from PhysicsTools.NanoAODTools.postprocessing.modules.common.collectionMerger import collectionMerger
 from LatinoAnalysis.NanoGardener.framework.BranchMapping import mappedOutputTree, mappedEvent
-
 
 import os.path
 
@@ -76,12 +78,47 @@ class KinFitterProducer(Module):
           self._DeepB_WP_M = 0.4941
         elif int(Year) == 2018:
           self._DeepB_WP_M = 0.4184
+
+        # read jet energy resolution (JER) and JER scale factors and uncertainties
+        # (the txt files were downloaded from https://github.com/cms-jet/JRDatabase/tree/master/textFiles/ )
+        # Text files are now tarred so must extract first
+        #
+
+        if int(Year) == 2016:
+          jerInputFileName = "Summer16_25nsV1_DATA_PtResolution_AK4PFchs.txt"
+        elif int(Year) == 2017:
+          jerInputFileName = "Fall17_V3_DATA_PtResolution_AK4PFchs.txt"
+        elif int(Year) == 2018:
+          jerInputFileName = "Autumn18_V7_DATA_PtResolution_AK4PFchs.txt"
+        #
+        self.jerInputArchivePath = os.environ['CMSSW_BASE'] + "/src/PhysicsTools/NanoAODTools/data/jme/"
+        self.jerTag = jerInputFileName[:jerInputFileName.find('_DATA_')+len('_DATA')]
+        self.jerArchive = tarfile.open(self.jerInputArchivePath+self.jerTag+".tgz", "r:gz")
+        self.jerInputFilePath = tempfile.mkdtemp()
+        self.jerArchive.extractall(self.jerInputFilePath)
+        self.jerInputFileName = jerInputFileName
+        
+        #self.jmr_vals = jmr_vals
+        
+        self.params_sf_and_uncertainty = ROOT.PyJetParametersWrapper()
+        self.params_resolution = ROOT.PyJetParametersWrapper()
+        
+        # load libraries for accessing JER scale factors and uncertainties from txt files
+        for library in [ "libCondFormatsJetMETObjects", "libPhysicsToolsNanoAODTools" ]:
+            if library not in ROOT.gSystem.GetLibraries():
+                print("Load Library '%s'" % library.replace("lib", ""))
+                ROOT.gSystem.Load(library)
+
       
     def beginJob(self):
-        pass
+        # initialize JER scale factors and uncertainties
+        # (cf. PhysicsTools/PatUtils/interface/SmearedJetProducerT.h )
+        print("Loading jet energy resolutions (JER) from file '%s'" % os.path.join(self.jerInputFilePath, self.jerInputFileName))
+        self.jer = ROOT.PyJetResolutionWrapper(os.path.join(self.jerInputFilePath, self.jerInputFileName))
+
 
     def endJob(self):
-        pass
+        shutil.rmtree(self.jerInputFilePath)
 
     def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         self.out = mappedOutputTree(wrappedOutputTree, mapname=self._branch_map)
@@ -125,8 +162,9 @@ class KinFitterProducer(Module):
         if nJet < 4:
           return False
 
-        jets     = ROOT.std.vector(ROOT.TLorentzVector)(0)
-        btag_vector = ROOT.std.vector(bool)(0)
+        jets            = ROOT.std.vector(ROOT.TLorentzVector)(0)
+        #jetPtResolution = ROOT.std.vector(float)(0)
+        btag_vector     = ROOT.std.vector(bool)(0)
 
         nbtags = 0
         njets  = 0
@@ -138,6 +176,9 @@ class KinFitterProducer(Module):
           tmp_jet = ROOT.TLorentzVector()
           tmp_jet.SetPtEtaPhiM(jet.pt, jet.eta, jet.phi, OrigJet[jet.jetIdx].mass)
           jets.push_back(tmp_jet)
+          jetPtResolution_ = self.getJetPtResolution(tmp_jet, event.fixedGridRhoFastjetAll)
+          print("jetPtResolution :", jetPtResolution_)
+          #jetPtResolution.push_back(self.)
           #print(jet.pt,"   ",jet.eta,"    ",jet.phi)
           if OrigJet[jet.jetIdx].btagDeepB > self._DeepB_WP_M:
             btag_vector.push_back(True)
@@ -171,12 +212,21 @@ class KinFitterProducer(Module):
         for nameBranches in self.newbranches :
           self.out.fillBranch(nameBranches  ,  variables[nameBranches]);
 
-        #del jets
-        #del btag_vector
-        #del lepton
-        #del MET
-        #del METShiftX
-        #del METShiftY
-        
         return True
 
+
+    def getJetPtResolution(self, jetIn, rho):
+        if hasattr( jetIn, "p4"):
+          jet = jetIn.p4()
+        else:
+          jet = jetIn
+
+        if not (jet.Perp() > 0.):
+            print("WARNING: jet pT = %1.1f !!" % jet.Perp())
+            return ( jet.Perp(), jet.Perp(), jet.Perp() )
+
+        self.params_resolution.setJetPt(jet.Perp())
+        self.params_resolution.setJetEta(jet.Eta())
+        self.params_resolution.setRho(rho)
+        jet_pt_resolution = self.jer.getResolution(self.params_resolution)
+        return jet_pt_resolution
