32c7ea792b71087fb581a835cea3da16ba335cec
your name your last name <soarnsoar@gmail.com>
2020-03-17 22:27:33 +0900

    update HMlnjjVars_Dev_jhchoi with algorithm

---
+++ NanoGardenerModules/HMlnjjVars_Dev_jhchoi/lnToModule.sh
@@ -0,0 +1,3 @@
+#ln -s ${PWD}/HMlnjjVars_Dev_jhchoi.py ${CMSSW_BASE}/src/LatinoAnalysis/NanoGardener/python/modules/HMlnjjVars_Dev_jhchoi.py
+#ln -s ${PWD}/HMlnjjVars_Dev_jhchoi3.py ${CMSSW_BASE}/src/LatinoAnalysis/NanoGardener/python/modules/HMlnjjVars_Dev_jhchoi3.py
+ln -s ${PWD}/HMlnjjVars_Dev_jhchoi4.py ${CMSSW_BASE}/src/LatinoAnalysis/NanoGardener/python/modules/HMlnjjVars_Dev_jhchoi4.py
