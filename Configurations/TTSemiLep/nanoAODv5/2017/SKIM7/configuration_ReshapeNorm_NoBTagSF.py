c720d2aec20b163afe252d408653e226741df1eb
ByungHun Oh <bh.oh@cern.ch>
2020-05-07 09:38:29 +0900

    2017 configuration

---
+++ Configurations/TTSemiLep/nanoAODv5/2017/SKIM7/configuration_ReshapeNorm_NoBTagSF.py
@@ -0,0 +1,26 @@
+##Just set below things##
+
+
+tag='2018_ReshapeNorm_NoBTagSF'
+
+variablesFile='ReshapeNorm_NoBTagSF/variables.py' ##what variables to draw
+cutsFile='ReshapeNorm_NoBTagSF/cuts.py' ## event selection##region selection
+plotFile='plot.py' ##color code and some format-related things
+
+samplesFile = 'ReshapeNorm_NoBTagSF/samples_2017.py'
+
+lumi=59.7
+
+#outputDirPlots='plots_'+tag+'Blined'
+outputDirPlots='plots_'+tag
+outputDir =           'rootFile_'+tag
+treeName='Events'
+aliasesFile='aliases.py'
+nuisancesFile = 'nuisances.py'
+
+#structureFile = 'structure.py'
+
+
+#outputDirDatacard='DataCards'
+
+maxLogCratio=10000
