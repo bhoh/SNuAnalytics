4c076f752bd42c3fd2d5438ca483673e7cdeb897
your name your last name <soarnsoar@gmail.com>
2020-05-12 12:25:01 +0900

    add README

---
+++ Configurations/HWWSemiLepHighMass/nanoAODv5/v6_production/2016/SKIM10/HMVAR10_nominal/WPandCut2016.py
@@ -0,0 +1,53 @@
+import os
+##---
+configurations = '%s/src/SNuAnalytics/Configurations/HWWSemiLepHighMass/data/' % os.getenv('CMSSW_BASE')
+os.system('cp '+configurations+'/Wtagger_cfg.py .')
+os.system('cp '+configurations+'/Wjjtagger_cfg.py .')
+
+from Wtagger_cfg import WJID
+from Wjjtagger_cfg import MYALGO,JETCUTS
+Year='2016'
+eleWP='mva_90p_Iso2016'
+muWP='cut_Tight80x'
+
+bAlgo='DeepB'
+bWP='0.2217'
+btagSFSource = '%s/src/PhysicsTools/NanoAODTools/data/btagSF/DeepCSV_2016LegacySF_V1.csv' % os.getenv('CMSSW_BASE')
+
+elePtCut='30'
+muPtCut='30'
+
+ALGO="dMchi2Resolution"
+WTAG="HP40"
+
+SFweight='puWeight*EMTFbug_veto*PrefireWeight*trigWeight*LepWPweight*LepWPCut*btagSF*PUJetIdSF*tau21SFnom'
+
+tau21SF=str(WJID['2016'][WTAG]['effSF']['nom'])
+tau21SFup=str(WJID['2016'][WTAG]['effSF']['up'])
+tau21SFdown=str(WJID['2016'][WTAG]['effSF']['down'])
+
+jetptmin=str(JETCUTS['2016']['ptmin'])
+jetetamax=str(JETCUTS['2016']['etamax'])
+
+##--MEKD model Mass & C to scan
+MELA_MASS_BOOST=[400,900,1500]
+MELA_C_BOOST=['1','0.1','0.01','0.001','0.0001','0.00001','0.000001','0.0000001','0.00000001']
+
+##---MEKD setup for final
+MELA_MASS_BOOST_WP=1500
+MELA_C_BOOST_WP='0.0001'
+
+
+##--MEKD model Mass & C to scan
+MELA_MASS_RESOL=[200,400]
+MELA_C_RESOL=['1','0.1','0.01','0.001','0.0001','0.00001','0.000001','0.0000001','0.00000001']
+
+##---MEKD setup for final
+MELA_MASS_RESOL_WP=400
+MELA_C_RESOL_WP='0.001'
+
+
+
+METtype="PuppiMET"
+METcutBst='40'
+METcutRes='30'
