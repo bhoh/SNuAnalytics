ff776d9f75cacdbdd8f31db63a132650ed710840
SangEun Lee <d4space@gmail.com>
2020-05-13 15:52:47 +0900

    haha

---
+++ Configurations/HWWSemiLepHighMass/nAODv5v6_MVA/mvaeffscxxMod.C
@@ -0,0 +1,1 @@
+../../../MVATool/scripts/mvaeffscxxMod.C
--- Configurations/HWWSemiLepHighMass/nAODv5v6_MVA/run.sh
+++ Configurations/HWWSemiLepHighMass/nAODv5v6_MVA/run.sh
@@ -1,2 +1,8 @@
-python test.py
+
+# To make the tree for MELA cut study
+#python test.py
 #python test.py | tee log.txt
+
+# Copy MELA cut tree which is the output of TMVA cuts
+#scp -r kistiUi20:Latino/CMSSW10215pch2/src/SNuAnalytics/Configurations/HWWSemiLepHighMass/nAODv5v6_MVA/Out_Roots .
+scp -r kistiUi20:Latino/CMSSW10215pch2/src/SNuAnalytics/Configurations/HWWSemiLepHighMass/nAODv5v6_MVA/Logs .
