3afdbc85d88b00dad7f00aaad164798e8f6e8738
ByungHun Oh <bh.oh@cern.ch>
2020-06-01 08:40:38 +0900

    chtocb plot config 2016

---
+++ Configurations/TTSemiLep/nanoAODv5/2016/StackNew_comb/template/configuration_withSig.py
@@ -0,0 +1,28 @@
+##Just set below things##
+
+
+tag='2016_SKIM7<CH>'
+
+variablesFile='variables<CH>.py' ##what variables to draw
+cutsFile='cuts<CH>.py' ## event selection##region selection
+plotFile='plot<CH>.py' ##color code and some format-related things
+
+samplesFile = 'samples_2016.py'
+
+lumi=35.9
+
+#outputDirPlots='plots_'+tag+'Blined'
+outputDirPlots='plots_'+tag
+outputDir =           'rootFile_'+tag
+treeName='Events'
+aliasesFile='aliases.py'
+#nuisancesFile = 'nuisances_stat_only.py'
+nuisancesFile = 'nuisances_all<CH>.py'
+#nuisancesFile = 'nuisances_all.py'
+
+#structureFile = 'structure.py'
+
+
+#outputDirDatacard='DataCards'
+
+maxLogCratio=10000
