78cfa72bae09e8bbb9877cf92e4786757b480d8e
ByungHun Oh <bh.oh@cern.ch>
2020-06-01 13:41:13 +0900

    updates for CHToCB

--- PostProcess/ChargedHiggsToCB/2017/run_signal.sh
+++ PostProcess/ChargedHiggsToCB/2017/run_signal.sh
@@ -26,7 +26,7 @@ SAMPLE_LIST=''
 EXCLUDE_LIST=''
 for s in ${SAMPLES[@]};do SAMPLE_LIST=${s}','${SAMPLE_LIST};done
 for e in ${EXCLUDE[@]};do EXCLUDE_LIST=${e}','${EXCLUDE_LIST};done
-#echo ${SAMPLE_LIST}
+echo ${SAMPLE_LIST}
 #echo ${EXCLUDE_LIST}
 
 ######################
@@ -34,13 +34,19 @@ for e in ${EXCLUDE[@]};do EXCLUDE_LIST=${e}','${EXCLUDE_LIST};done
 ######################
 
 sitescfg='--sitescfg SNuAnalytics/PostProcess/ChargedHiggsToCB/Sites_cfg.py'
-modcfg='--modcfg SNuAnalytics/PostProcess/ChargedHiggsToCB/Steps_cfg.py'
+#modcfg='--modcfg SNuAnalytics/NanoGardenerFrameworks/Steps_cfg.py'
+modcfg='--modcfg SNuAnalytics/NanoGardenerFrameworks/HWWSemilepHM/20200406_HMSemilepSKIMv6_10/Steps_cfg.py'
 datacfg='--datacfg SNuAnalytics/PostProcess/ChargedHiggsToCB/Productions_cfg.py'
 
 #--l1Prod--#
-mkPostProc.py ${sitescfg} ${modcfg} ${datacfg} -p Fall2017_102X_nAODv5_Full2017v6 -i Prod -s MCl1loose2017v6 -b -T ${SAMPLE_LIST}
+#mkPostProc.py ${sitescfg} ${modcfg} ${datacfg} -p Fall2017_102X_nAODv5_Full2017v6 -i Prod -s MCl1loose2017v6 -b -T ${SAMPLE_LIST}
+#mkPostProc.py ${sitescfg} ${datacfg} -p Fall2017_102X_nAODv5_Full2017v6 -i Prod -s MCl1loose2017v6 -b -T CHToCB_M140
 #--Corr--#
-#mkPostProc.py ${sitescfg} ${modcfg} ${datacfg} -p Fall2017_102X_nAODv5_Full2017v6 -i MCl1loose2017v5 -s MCCorr2017v5 -b -T ${SAMPLE_LIST} 
+#mkPostProc.py ${sitescfg} ${modcfg} ${datacfg} -p Fall2017_102X_nAODv5_Full2017v6 -i MCl1loose2017v6 -s MCCorr2017v6 -b -T ${SAMPLE_LIST} 
+#mkPostProc.py ${sitescfg} ${datacfg} -p Fall2017_102X_nAODv5_Full2017v6 -i MCl1loose2017v6 -s MCCorr2017v6 -b -T CHToCB_M140
+#--HM--#
+#mkPostProc.py ${sitescfg} ${modcfg} ${datacfg} -p Fall2017_102X_nAODv5_Full2017v6 -i MCl1loose2017v6__MCCorr2017v6 -s HMSemilepSKIMv6_10 -b -T ${SAMPLE_LIST}
+mkPostProc.py ${sitescfg} ${modcfg} ${datacfg} -p Fall2017_102X_nAODv5_Full2017v6 -i MCl1loose2017v6__MCCorr2017v6 -s HMSemilepSKIMv6_10 -b -T CHToCB_M140
 
 
 #mkPostProc.py ${sitescfg} ${modcfg} ${datacfg} -p Fall2017_102X_nAODv5_Full2017v6 -i MCl1loose2017v5__MCCorr2017v5 -s Semilep2017 -b -T ${SAMPLE_LIST}
