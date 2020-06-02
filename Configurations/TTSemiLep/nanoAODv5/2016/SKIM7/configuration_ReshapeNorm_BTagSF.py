873e62cd5655cf5db0b12299f6580f655dfe19ab
ByungHun Oh <bh.oh@cern.ch>
2020-06-02 13:02:05 +0900

    2016 btag reshape norm

--- Configurations/TTSemiLep/nanoAODv5/2016/SKIM7/configuration_ReshapeNorm_BTagSF.py
+++ Configurations/TTSemiLep/nanoAODv5/2016/SKIM7/configuration_ReshapeNorm_BTagSF.py
@@ -1,7 +1,7 @@
 ##Just set below things##
 
 
-tag='2018_ReshapeNorm_BTagSF'
+tag='2016_ReshapeNorm_BTagSF'
 
 variablesFile='ReshapeNorm_BTagSF/variables.py' ##what variables to draw
 cutsFile='ReshapeNorm_BTagSF/cuts.py' ## event selection##region selection
@@ -9,14 +9,14 @@ plotFile='plot.py' ##color code and some format-related things
 
 samplesFile = 'ReshapeNorm_BTagSF/samples_2016.py'
 
-lumi=59.7
+lumi=35.9
 
 #outputDirPlots='plots_'+tag+'Blined'
 outputDirPlots='plots_'+tag
 outputDir =           'rootFile_'+tag
 treeName='Events'
 aliasesFile='aliases.py'
-nuisancesFile = 'nuisances.py'
+nuisancesFile = 'nuisances_NULL.py'
 
 #structureFile = 'structure.py'
 
--- Configurations/TTSemiLep/nanoAODv5/2016/SKIM7/configuration_ReshapeNorm_NoBTagSF.py
+++ Configurations/TTSemiLep/nanoAODv5/2016/SKIM7/configuration_ReshapeNorm_NoBTagSF.py
@@ -1,7 +1,7 @@
 ##Just set below things##
 
 
-tag='2018_ReshapeNorm_NoBTagSF'
+tag='2016_ReshapeNorm_NoBTagSF'
 
 variablesFile='ReshapeNorm_NoBTagSF/variables.py' ##what variables to draw
 cutsFile='ReshapeNorm_NoBTagSF/cuts.py' ## event selection##region selection
@@ -9,14 +9,14 @@ plotFile='plot.py' ##color code and some format-related things
 
 samplesFile = 'ReshapeNorm_NoBTagSF/samples_2016.py'
 
-lumi=59.7
+lumi=35.9
 
 #outputDirPlots='plots_'+tag+'Blined'
 outputDirPlots='plots_'+tag
 outputDir =           'rootFile_'+tag
 treeName='Events'
 aliasesFile='aliases.py'
-nuisancesFile = 'nuisances.py'
+nuisancesFile = 'nuisances_NULL.py'
 
 #structureFile = 'structure.py'
 
