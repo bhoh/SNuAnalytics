485dbda1a85cdbc7edb83fb27821ef7077e4f3b6
your name your last name <soarnsoar@gmail.com>
2020-06-11 07:33:09 +0900

    combine scripts

---
+++ Combinato/HighMassH/1st/Datacards_2017/make_workspace.sh
@@ -0,0 +1,1 @@
+text2workspace.py $1 -P HiggsAnalysis.CombinedLimit.HiggsCombinePhysicsModel.HighMassScalar.HighMassScalarSemilepNoI:GGFONLY --mass 1000
---
+++ Combinato/HighMassH/1st/Datacards_2017/run_impact_plot.sh
@@ -0,0 +1,22 @@
+##http://cms-analysis.github.io/HiggsAnalysis-CombinedLimit/part3/nonstandard/
+##TODO
+##-t -1  : make toy using expectation
+##toy frequentist : fit to data
+##-
+
+###$1 -> workspace file
+#robust="--robustFit 1"
+robust=""
+#opt="--cminFallbackAlgo Minuit2,Simplex,1:10" 
+opt="--cminFallbackAlgo Minuit2,Simplex,0:100"
+#opt="--cminFallbackAlgo Minuit2,Migrad,0:1.0"
+combineTool.py -M Impacts -d $1 -m 1000 --doInitialFit $robust $opt
+
+
+combineTool.py -M Impacts -d $1 -m 1000 $robust --doFits $opt
+
+
+combineTool.py -M Impacts -d $1 -m 1000 -o impacts.json
+
+
+plotImpacts.py -i impacts.json -o impacts --blind
