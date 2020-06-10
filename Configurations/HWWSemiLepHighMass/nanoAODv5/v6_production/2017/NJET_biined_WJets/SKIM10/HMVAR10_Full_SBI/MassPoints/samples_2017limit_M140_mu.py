9dd0e329f3924b4515329c9b1a6f4c902ba356fb
your name your last name <soarnsoar@gmail.com>
2020-06-10 16:32:52 +0900

    add Mass points

---
+++ Configurations/HWWSemiLepHighMass/nanoAODv5/v6_production/2017/NJET_biined_WJets/SKIM10/HMVAR10_Full_SBI/MassPoints/samples_2017limit_M140_mu.py
@@ -0,0 +1,11 @@
+samples["DY"]={}
+samples["MultiV"]={}
+samples["WpWmJJ_EWK_QCD_noHiggs"]={}
+samples["top"]={}
+samples["Wjets"]={}
+samples["vbfHWWlnuqq_M125"]={}
+samples["ggHWWlnuqq_M125"]={}
+samples["QCD_MU"]={}
+samples["DATA"]={}
+samples["ggHWWlnuqq_M140"]={}
+samples["vbfHWWlnuqq_M140"]={}
