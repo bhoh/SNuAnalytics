7fd65b82ca20379545661e3b0ae62ad11b4dfef8
SangEun Lee <d4space@gmail.com>
2020-06-16 22:42:36 +0900

    haha

--- MVATool/python/KerasModel.py
+++ MVATool/python/KerasModel.py
@@ -1,8 +1,10 @@
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation, Dropout
+from keras.regularizers import l2
 from keras.layers.normalization import BatchNormalization
 from keras import initializers
 from keras.optimizers import SGD
+from keras.utils import plot_model
 
 # Define initialization
 def normal(shape, name=None):
@@ -44,12 +46,27 @@ class KerasModel():
     #self.model.add(Dense(32, kernel_initializer=initializers.he_normal(seed=None), activation='relu'))
     #self.model.add(Dense(2, kernel_initializer=initializers.he_normal(seed=None), activation='softmax'))
 
-  def compile(self,loss_='categorical_crossentropy',
-		   optimizer_=SGD(lr=0.1,decay=1e-5),
-		   metrics_=['accuracy',]
+  def setHiddenLayers(self, n_Nodes_Input, n_Nodes_Hidden, n_hidden_Layers, activ='relu', l2_val = 1e-5):
+    #self.model.add( Dense( n_Nodes_Hidden, activation=activ, W_regularizer=l2(l2_val), input_dim = n_Nodes_Input) )
+    self.model.add( Dense( n_Nodes_Hidden, activation=activ, input_dim = n_Nodes_Input) )
+    for k in range(n_hidden_Layers - 1):
+      #self.model.add( Dense( n_Nodes_Hidden, activation=activ, W_regularizer=l2(l2_val))  )
+      self.model.add( Dense( n_Nodes_Hidden, activation=activ)  )
+
+  def setOutLayer(self, n_Nodes_Out, activ = 'softmax'):
+    # Output layer
+    # NOTE: Use following output types for the different tasks
+    # Binary classification: 2 output nodes with 'softmax' activation
+    # Regression: 1 output with any activation ('linear' recommended)
+    # Multiclass classification: (number of classes) output nodes with 'softmax' activation
+    self.model.add(Dense(n_Nodes_Out, activation=activ))
+
+  def compile(self,lossftn='categorical_crossentropy',
+		   SGD_lr=0.1, SGD_decay=1e-5,
+		   metrix=['accuracy',]
 	     ):
     # Set loss and optimizer
-    self.model.compile(loss=loss_, optimizer=optimizer_, metrics=metrics_)
+    self.model.compile(loss=lossftn, optimizer=SGD(SGD_lr, SGD_decay), metrics=metrix)
 
   def save(self, modelName="model.h5"):
     self.model.save(modelName)
@@ -57,5 +74,11 @@ class KerasModel():
   def summary(self):
     self.model.summary()
 
+  def plot_mymodel(self,outFile='model.png'):
+    print 'plot model............'
+    try:
+      plot_model(self.model,  to_file=outFile, show_shapes = False)
+    except:
+      print('[INFO] Failed to make model plot')
 	       
 		  
--- MVATool/python/MLTools.py
+++ MVATool/python/MLTools.py
@@ -7,44 +7,60 @@ import logging
 
 class MLTools():
 
-  def __init__(self):
-    self._tools = None
+  def __init__(self, Config):
+    self._trees = {}
+    self._fout = None
+    self._variables = {}
+    self._spectators = {}
+    self._cuts = {}
+    self._cfgs = Config
+    self._debug = True
 
   def __del__(self):
     pass
 
-  def SetMLTools(self,tools):
-    self._tools = tools()
+  #def SetMLTools(self,tools):
+  #  self._tools = tools()
+
+  def InitTreeVariableCuts(self):
+    self._trees = {}
+    self._fout = None
+    self._variables = {}
+    self._spectators = {}
+    self._cuts = {}
 
   def SetTrees(self,sampleName,tree_name,inFiles):
     inputDir = ''
     # get chained ttree
     self._treeName = "%s_%s"%(sampleName,tree_name)
     trees = self._connectInputs( inFiles, inputDir, tree_name,False)
-    self._tools.SetTrees(self._treeName, trees)
+    self._trees[self._treeName]= trees
 
-  def SetVariables(self, variables):
-    self._tools.SetVariables(variables)
-  def SetSpectators(self, spector):
-    self._tools.SetSpectators(spector)
+  def SetVariables(self,variables):
+    self._variables = variables
+
+  def SetSpectators(self,spectors):
+    self._spectators = spectors
+
 
   def SetCuts(self,cuts):
-    self._tools.SetCuts(cuts)
+    self._cuts = cuts
 
-  def SetOptions(self,options):
-    self._tools.SetOptions(options)
+
+  #def SetConfigs(self,Configs):
+  #  self._tools.SetConfigs(Configs)
     
-  def doTrain(self, sigTreeName, bkgTreeName, outWeightsSuffix, outFileName, epoch=1):
-    self._tools.doTrain( sigTreeName, bkgTreeName, outWeightsSuffix, outFileName, epoch)
-    #os.system('mv TMVAClassification/weights TMVAClassification/weights_%s'%(outWeightsSuffix))
+  #def doTrain(self, sigTreeName, bkgTreeName, outWeightsSuffix, outFileName, epoch=1):
+  #  self._tools.doTrain( sigTreeName, bkgTreeName, outWeightsSuffix, outFileName, epoch)
+  #  #os.system('mv TMVAClassification/weights TMVAClassification/weights_%s'%(outWeightsSuffix))
 
-  def doTest(self):
-    self._tools.doTest()
+  #def doTest(self):
+  #  self._tools.doTest()
 
   def _connectInputs(self, inFiles, inputDir, tree_name, skipMissingFiles, friendsDir = None, skimListDir = None):
     tree = TChain(tree_name)
     for aFile in inFiles:
-      print(aFile)
+      #print(aFile)
       tree.Add(aFile)
     return tree
 
