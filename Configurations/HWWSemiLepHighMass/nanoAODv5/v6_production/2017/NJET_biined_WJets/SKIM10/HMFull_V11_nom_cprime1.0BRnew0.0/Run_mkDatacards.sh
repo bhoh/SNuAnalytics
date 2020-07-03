1b3da4ce4dec6f047fb89097b1844b98c324dfb8
your name your last name <soarnsoar@gmail.com>
2020-07-03 23:42:47 +0900

    Using batch for mkDatacard

--- Configurations/HWWSemiLepHighMass/nanoAODv5/v6_production/2017/NJET_biined_WJets/SKIM10/HMFull_V11_nom_cprime1.0BRnew0.0/Run_mkDatacards.sh
+++ Configurations/HWWSemiLepHighMass/nanoAODv5/v6_production/2017/NJET_biined_WJets/SKIM10/HMFull_V11_nom_cprime1.0BRnew0.0/Run_mkDatacards.sh
@@ -1,3 +1,4 @@
+CURDIR=$PWD
 StartTime=$(date +%s)
 
 python TurnOnCombinedSamples.py WPandCut2017.py CombineMultiV
@@ -28,7 +29,7 @@ ARR_MASS=$DefineList
 #inputBoost=`ls rootFile*Boost*/hadd.root`
 #inputResol=`ls rootFile*Resol*/hadd.root`
 
-ARR_MASS=(1000)
+#ARR_MASS=(1000)
 TOTAL_MX=""
 for MX in ${ARR_MASS[@]};do
     TOTAL_MX="$TOTAL_MX,${MX}"
@@ -42,6 +43,7 @@ ListFlavor=(mu ele)
 #ListRegion=(TOP SB SR)
 ListRegion=(SR CR)
 #ListProc=(VBF GGF)
+#mkdir -p 
 for MX in ${ARR_MASS[@]};do
     
     for flv in ${ListFlavor[@]};do
@@ -52,29 +54,39 @@ for MX in ${ARR_MASS[@]};do
 	    input=`ls rootFile*Boosted*/hadd.root`
 	    cp cuts_Boosted.py cuts_Boosted_${flv}_${rg}.py
 
-	    sleep 1
+	    #sleep 1
 
-	    mkDatacards.py --pycfg=configuration_Boosted.py --structureFile=MassPoints/structure_M${MX}_${flv}_${rg}.py --cutsFile=cuts_Boosted_${flv}_${rg}.py --inputFile=$input --outputDirDatacard=Datacards_${YEAR}/Datacard_M${MX} --samplesFile=MassPoints/samples_${YEAR}limit_M${MX}_${flv}_${rg}.py --variablesFile=variables_Boosted_Final.py &> logs/Run_mkDatacards_cuts_Boosted_${flv}_M${MX}_${rg}.log&
+	    #mkDatacards.py --pycfg=configuration_Boosted.py --structureFile=MassPoints/structure_M${MX}_${flv}_${rg}.py --cutsFile=cuts_Boosted_${flv}_${rg}.py --inputFile=$input --outputDirDatacard=Datacards_${YEAR}/Datacard_M${MX} --samplesFile=MassPoints/samples_${YEAR}limit_M${MX}_${flv}_${rg}.py --variablesFile=variables_Boosted_Final.py &> logs/Run_mkDatacards_cuts_Boosted_${flv}_M${MX}_${rg}.log&
+	    #command="cd ${CURDIR};mkDatacards.py --pycfg=configuration_Boosted.py --structureFile=MassPoints/structure_M${MX}_${flv}_${rg}.py --cutsFile=cuts_Boosted_${flv}_${rg}.py --inputFile=$input --outputDirDatacard=Datacards_${YEAR}/Datacard_M${MX} --samplesFile=MassPoints/samples_${YEAR}limit_M${MX}_${flv}_${rg}.py --variablesFile=variables_Boosted_Final.py &> logs/Run_mkDatacards_cuts_Boosted_${flv}_M${MX}_${rg}.log"
+	    command="cd ${CURDIR};mkDatacards.py --pycfg=configuration_Boosted.py --structureFile=MassPoints/structure_M${MX}_${flv}_${rg}.py --cutsFile=cuts_Boosted_${flv}_${rg}.py --inputFile=$input --outputDirDatacard=Datacards_${YEAR}/Datacard_M${MX} --samplesFile=MassPoints/samples_${YEAR}limit_M${MX}_${flv}_${rg}.py --variablesFile=variables_Boosted_Final.py"
+	    python python_tool/ExportShellCondorSetup.py -c "$command" -d "Run_mkDatacards/Boosted_M${MX}_${flv}_${rg}/" -n "Run_mkDatacards_M${MX}_${flv}_${rg}_Boosted" -s
+	    
 
-	    sleep 1
+	    #sleep 1
 	
 	    input=`ls rootFile*Resolved*/hadd.root`
 	    #echo ${input}
 	    cp cuts_Resolved.py cuts_Resolved_${flv}_${rg}.py
 
-	    sleep 1
+	    #sleep 1
 
-	    mkDatacards.py --pycfg=configuration_Resolved.py --structureFile=MassPoints/structure_M${MX}_${flv}_${rg}.py --cutsFile=cuts_Resolved_${flv}_${rg}.py --inputFile=$input --outputDirDatacard=Datacards_${YEAR}/Datacard_M${MX} --samplesFile=MassPoints/samples_${YEAR}limit_M${MX}_${flv}_${rg}.py --variablesFile=variables_Resolved_Final.py &> logs/Run_mkDatacards_cuts_Resolved_${flv}_M${MX}_${rg}.log&
+	    #mkDatacards.py --pycfg=configuration_Resolved.py --structureFile=MassPoints/structure_M${MX}_${flv}_${rg}.py --cutsFile=cuts_Resolved_${flv}_${rg}.py --inputFile=$input --outputDirDatacard=Datacards_${YEAR}/Datacard_M${MX} --samplesFile=MassPoints/samples_${YEAR}limit_M${MX}_${flv}_${rg}.py --variablesFile=variables_Resolved_Final.py &> logs/Run_mkDatacards_cuts_Resolved_${flv}_M${MX}_${rg}.log&
+
+	    #command="cd ${CURDIR};mkDatacards.py --pycfg=configuration_Resolved.py --structureFile=MassPoints/structure_M${MX}_${flv}_${rg}.py --cutsFile=cuts_Resolved_${flv}_${rg}.py --inputFile=$input --outputDirDatacard=Datacards_${YEAR}/Datacard_M${MX} --samplesFile=MassPoints/samples_${YEAR}limit_M${MX}_${flv}_${rg}.py --variablesFile=variables_Resolved_Final.py &> logs/Run_mkDatacards_cuts_Resolved_${flv}_M${MX}_${rg}.log"
+	    command="cd ${CURDIR};mkDatacards.py --pycfg=configuration_Resolved.py --structureFile=MassPoints/structure_M${MX}_${flv}_${rg}.py --cutsFile=cuts_Resolved_${flv}_${rg}.py --inputFile=$input --outputDirDatacard=Datacards_${YEAR}/Datacard_M${MX} --samplesFile=MassPoints/samples_${YEAR}limit_M${MX}_${flv}_${rg}.py --variablesFile=variables_Resolved_Final.py"
+	    python python_tool/ExportShellCondorSetup.py -c "$command" -d "Run_mkDatacards/Resolved_M${MX}_${flv}_${rg}/" -n "Run_mkDatacards_M${MX}_${flv}_${rg}_Resolved" -s
+
+
 		
-	    sleep 1
-	    while [ 1 ];do
-		NJOB=`jobs|wc -l`
-		if [ $NJOB -lt ${NSPLIT} ];then
-                    echo "NJOB < ${NSPLIT}"
-                    break
-		fi
-		sleep 3
-            done
+	    #sleep 1
+	    #while [ 1 ];do
+	    #NJOB=`jobs|wc -l`
+		#if [ $NJOB -lt ${NSPLIT} ];then
+                #    echo "NJOB < ${NSPLIT}"
+                #    break
+		#fi
+		#sleep 3
+            #done
 	done
 
 	
