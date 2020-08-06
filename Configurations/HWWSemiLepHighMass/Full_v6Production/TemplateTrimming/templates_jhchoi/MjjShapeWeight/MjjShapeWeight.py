883205e786e0a89af4e1f5106c2d36e84a474d7f
your name your last name <soarnsoar@gmail.com>
2020-08-06 17:48:04 +0900

    Trimming code

---
+++ Configurations/HWWSemiLepHighMass/Full_v6Production/TemplateTrimming/templates_jhchoi/MjjShapeWeight/MjjShapeWeight.py
@@ -0,0 +1,63 @@
+DICT_MjjShapeW={
+    '2018':{
+        'Boosted':{
+            'w_min':1,
+            'w_max':1
+        },
+        #w_min :    +0.666   -0.013/+0.014 (68%)
+        #w_max :    +1.151   -0.009/+0.009 (68%)
+
+        'Resolved':{
+            'w_min':0.666,
+            'w_max':1.151
+        },
+
+    },
+
+    '2017':{
+
+        'Boosted':{
+            'w_min':1,
+            'w_max':1
+        },
+
+        'Resolved':{
+            'w_min':0.741,
+            'w_max':1.073,
+            'w_min_err':0.018,
+            'w_max_err':0.011,
+
+        },
+
+        
+    },
+    
+    '2016':{
+
+        'Boosted':{
+            'w_min':1,
+            'w_max':1
+        },
+
+        'Resolved':{
+            'w_min':0.848,
+            'w_max':1.017,
+            'w_min_err':0.016,
+            'w_max_err':0.010,
+        },
+
+        
+    },
+    
+}
+for Year in DICT_MjjShapeW:
+    for bst in DICT_MjjShapeW[Year]:
+        w_min=DICT_MjjShapeW[Year][bst]['w_min']
+        w_max=DICT_MjjShapeW[Year][bst]['w_max']
+        
+        DICT_MjjShapeW[Year][bst]['slope']=w_max-w_min
+        DICT_MjjShapeW[Year][bst]['intercept']=w_min
+
+
+        #print DICT_MjjShapeW[Year][bst]['slope']
+        #print DICT_MjjShapeW[Year][bst]['intercept']
---
+++ Configurations/HWWSemiLepHighMass/Full_v6Production/TemplateTrimming/templates_jhchoi/script/Histo_factory_run_test_standalone.sh
@@ -0,0 +1,64 @@
+mkdir -p logs/
+StartTime=$(date +%s)
+
+python TurnOffCombinedSamples.py WPandCut2016.py CombineMultiV
+python TurnOffCombinedSamples.py WPandCut2016.py CombineH125
+python TurnOffCombinedSamples.py WPandCut2016.py CombineWjets
+python TurnOffCombinedSamples.py WPandCut2016.py Combine_ggWW
+python TurnOffCombinedSamples.py WPandCut2016.py Combine_qqWWqq
+python TurnOffCombinedSamples.py WPandCut2016.py CombineSBI
+
+
+
+
+
+cp nuisances.py nuisances_Boosted.py
+cp nuisances.py nuisances_Resolved.py
+
+
+#TEST
+#mkShapesMulti.py --pycfg=configuration_Boosted.py --batchSplit=AsMuchAsPossible --treeName Events
+#mkShapesMulti.py --pycfg=configuration_Resolved.py --batchSplit=AsMuchAsPossible --treeName Events
+
+#mkShapesMulti.py --pycfg=configuration_Boosted_GGF_SB.py --batchSplit=AsMuchAsPossible --treeName Events
+
+#Boosted
+#mkShapesMulti.py --pycfg=configuration_Boosted_SB.py --batchSplit=AsMuchAsPossible --doBatch --treeName Events
+#mkShapesMulti.py --pycfg=configuration_Boosted_TOP.py --batchSplit=AsMuchAsPossible --doBatch --treeName Events
+#mkShapesMulti.py --pycfg=configuration_Boosted_SR.py --batchSplit=AsMuchAsPossible --doBatch --treeName Events --samplesFile=samples_test.py
+
+#Resolved
+#mkShapesMulti.py --pycfg=configuration_Resolved_SB.py --batchSplit=AsMuchAsPossible --doBatch --treeName Events
+#mkShapesMulti.py --pycfg=configuration_Resolved_TOP.py --batchSplit=AsMuchAsPossible --doBatch --treeName Events
+mkShapesMulti.py --pycfg=configuration_Resolved_SR.py --treeName Events --samplesFile=samples_test.py
+#Boosted
+##--GGF
+#mkShapesMulti.py --pycfg=configuration_Boosted_GGF_SB.py --batchSplit=AsMuchAsPossible --doBatch --treeName Events
+#mkShapesMulti.py --pycfg=configuration_Boosted_GGF_TOP.py --batchSplit=AsMuchAsPossible --doBatch --treeName Events
+#mkShapesMulti.py --pycfg=configuration_Boosted_GGF_SR.py --batchSplit=AsMuchAsPossible --doBatch --treeName Events
+
+##--VBF
+#mkShapesMulti.py --pycfg=configuration_Boosted_VBF_SB.py --batchSplit=AsMuchAsPossible --doBatch --treeName Events
+#mkShapesMulti.py --pycfg=configuration_Boosted_VBF_TOP.py --batchSplit=AsMuchAsPossible --doBatch --treeName Events
+#mkShapesMulti.py --pycfg=configuration_Boosted_VBF_SR.py --batchSplit=AsMuchAsPossible --doBatch --treeName Events
+
+#Resolved
+##--GGF
+#mkShapesMulti.py --pycfg=configuration_Resolved_GGF_SB.py --batchSplit=AsMuchAsPossible --doBatch --treeName Events
+#mkShapesMulti.py --pycfg=configuration_Resolved_GGF_TOP.py --batchSplit=AsMuchAsPossible --doBatch --treeName Events
+#mkShapesMulti.py --pycfg=configuration_Resolved_GGF_SR.py --batchSplit=AsMuchAsPossible --doBatch --treeName Events
+
+##--VBF
+#mkShapesMulti.py --pycfg=configuration_Resolved_VBF_SB.py --batchSplit=AsMuchAsPossible --doBatch --treeName Events
+#mkShapesMulti.py --pycfg=configuration_Resolved_VBF_TOP.py --batchSplit=AsMuchAsPossible --doBatch --treeName Events
+#mkShapesMulti.py --pycfg=configuration_Resolved_VBF_SR.py --batchSplit=AsMuchAsPossible --doBatch --treeName Events
+
+
+
+EndTime=$(date +%s)
+echo $EndTime
+echo "runtime : $(($EndTime - $StartTime)) sec"
+echo -e "JOBDIR:${PWD}
+args=$@
+runtime=$(($EndTime - $StartTime))
+ScriptName=$BASH_SOURCE" | /bin/mailx -s "FINISHED JOB @ $HOSTNAME" $USER@cern.ch
